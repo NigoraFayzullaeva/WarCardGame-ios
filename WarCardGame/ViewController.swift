64d02e9e243eed800826d3288a65586f572be2e3
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-04-12 18:39:09 +0300

    UI done

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Apple on 10/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
